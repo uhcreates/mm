87c3cf7ceb8a18daa8ac6df63273e9d083aac206
codebunny23@ <codebunny23@gmail.com>
2018-12-10 12:29:06 +0530

    logo nav
    logo nav

--- index.php
+++ index.php
@@ -87,7 +87,8 @@ if ($gClient->getAccessToken()) {
     <meta charset="utf-8">
     <meta name="google-site-verification" content="cfEtbhLPxNxhejLF5MDgsRMIq-hapjeFth9hFCvLsN4" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/Pe-icon-7-stroke.css">
     <link rel="stylesheet" href="css/animate.css">
